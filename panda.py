913d1437419815141ea6e9c1361eca44396f6d44
Your Namerafael <you@example.comrafaeldesousacavalcante21@gmail.com>
2024-07-07 04:20:39 -0300

    07/07/2024

--- panda.py
+++ panda.py
@@ -1,4 +1,4 @@
-print("programa ano em idade em uma planilia")
+print("programa idade em uma planilia")
 import pandas as pd
 import datetime
 
@@ -14,15 +14,14 @@ nascimento = [2005, 1999, 2007]
 #pegando data atual
 relogio_atual = datetime.datetime.now()
 ano_atual = relogio_atual.year 
-data_atual = relogio_atual.month
+mes_atual = relogio_atual.month
 
 #criando coluna
 list = {'nome': nome, 'nascimento': nascimento, 'mes_nascimento' : mes_nascimento}
 
 df1 = pd.DataFrame(list)
-data = [data_atual]* len(df1['mes_nascimento'])
-print(data)
-df1['idade'] = (ano_atual - df1['nascimento']) - (df1['mes_nascimento'] > data)
+mes = [mes_atual]* len(df1['mes_nascimento'])
+df1['idade'] = (ano_atual - df1['nascimento']) - (df1['mes_nascimento'] > mes)
 
 
     
